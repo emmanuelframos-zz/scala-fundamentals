a928dd9d261fac45af09c07ca4b77298cfab2dfa
Emmanuel Ramos <emmanuelframos@gmail.com>
2019-03-19 22:44:02 -0300

    Lazy evaluation calling by name

---
+++ src/lectures/part1basics/CBNvsCBV.scala
@@ -0,0 +1,24 @@
+package lectures.part1basics
+
+object CBNvsCBV extends App {
+
+  def calledByValue(x: Long): Unit = {
+    println("by value: " + x)
+    println("by value: " + x)
+  }
+
+  def calledByName(x: => Long): Unit = {
+    println("by name: " + x)
+    println("by name: " + x)
+  }
+
+  calledByValue(System.nanoTime())
+  calledByName(System.nanoTime())
+
+  def infinite(): Int = 1 + infinite()
+  def printFirst(x: Int, y: => Int) = println(x)
+
+  printFirst(infinite(), 34) //causes stack overflow, because the first parameter is evaluated at every use within
+  printFirst(34, infinite()) //lazy evaluation avoid stack overflow, cause second parameter is not used
+
+}
